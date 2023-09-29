92710796dd3b43eafdc3de42032c254fea1ed43b
Abhisek Kar <abhi.originals.001@gmail.com>
2023-09-29 12:57:46 +0530

    installation of packages-tailwind postcss flowbite react-icons and configuration of added packages

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "flowbite-react";
+
+const App = () => {
+  return (
+    <div>
+      <Button color="black">Click me</Button>
+    </div>
+  );
+};
+
+export default App;
